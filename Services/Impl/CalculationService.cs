0e0b86a4f21460b756f55511a98aaabdfcf65d6b
dante72 <turbosuns7@gmail.com>
2025-07-17 03:41:54 +0300

    fix Mathematics

--- Services/Impl/CalculationService.cs
+++ Services/Impl/CalculationService.cs
@@ -1,6 +1,7 @@
 ﻿using EnergomerWebApp.Fields;
 using GeoCoordinatePortable;
-using System.Numerics;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.Mathematics;
 
 namespace EnergomerWebApp.Services.Impl
 {
@@ -9,75 +10,75 @@ namespace EnergomerWebApp.Services.Impl
         private const double EPS = 0.0001;
         public bool CheckField(Field field, GeoCoordinate point)
         {
-            var vectors = field.Locations.Polygon.Select(ToVector).ToArray();
-            var checkPoint = ToVector(point);
-            var sortedVectors = Sort(vectors);
+            var radiusVectors = field.Locations.Polygon.Select(ToVector).ToArray();
+            var currentRadiusVector = ToVector(point);
+            var sortedRadiusVectors = Sort(radiusVectors);
 
-            if (CheckCoplanarity(sortedVectors, checkPoint))
+            if (CheckCoplanarity(sortedRadiusVectors, currentRadiusVector))
             {
                 return true;
             }
 
-            var center = ToVector(field.Locations.Center);
+            var centerRadiusVector = ToVector(field.Locations.Center);
 
-            return PointIsInside(sortedVectors, checkPoint, center);
+            return PointIsInside(sortedRadiusVectors, currentRadiusVector, centerRadiusVector);
         }
 
-        private bool PointIsInside(Vector3[] vectors, Vector3 currentPoint, Vector3 center)
+        private bool PointIsInside(Vector3D[] radiusVectors, Vector3D currentRadiusVector, Vector3D centerRadiusVector)
         {
-            var points = CreateProjectionOnOXY(vectors);
-            var checkPoint = CreateProjectionOnOXY(currentPoint);
-            var centerPoint = CreateProjectionOnOXY(center);
+            var points = CreateProjectionOnOXY(radiusVectors);
+            var currentPoint = CreateProjectionOnOXY(currentRadiusVector);
+            var centerPoint = CreateProjectionOnOXY(centerRadiusVector);
             int crossPoints = 0;
 
             for (var i = 0; i < points.Count(); i++)
             {
-                if (CrossLines(points[i], points[(i + 1) % points.Count()], checkPoint, centerPoint) != null)
+                if (CrossLines(points[i], points[(i + 1) % points.Count()], currentPoint, centerPoint) != null)
                     crossPoints++;
             }
 
             return crossPoints == 1;
         }
 
-        private Vector2? CrossLines(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4)
+        private Point? CrossLines(Point p1, Point p2, Point p3, Point p4)
         {
-            float a1 = p2.Y - p1.Y;
-            float a2 = p4.Y - p3.Y;
+            double a1 = p2.Y - p1.Y;
+            double a2 = p4.Y - p3.Y;
 
-            float b1 = p1.X - p2.X;
-            float b2 = p3.X - p4.X;
+            double b1 = p1.X - p2.X;
+            double b2 = p3.X - p4.X;
 
-            float k = a1 * b2 - a2 * b1;
+            double k = a1 * b2 - a2 * b1;
 
             if (Math.Abs(k) > EPS)
             {
-                float c1 = p2.X * p1.Y - p1.X * p2.Y;
-                float c2 = p4.X * p3.Y - p3.X * p3.Y;
+                double c1 = p2.X * p1.Y - p1.X * p2.Y;
+                double c2 = p4.X * p3.Y - p3.X * p3.Y;
 
-                return new Vector2(-(c1 * b2 - c2 * b1) / k, -(a1 * c2 - a2 * c1) / k);
+                return new Point(-(c1 * b2 - c2 * b1) / k, -(a1 * c2 - a2 * c1) / k);
             }
 
             return null;
         }
 
-        private bool CheckCrossPoint(Vector2 p1, Vector2 p2, Vector2 result)
+        private bool CheckCrossPoint(Point p1, Point p2, Point result)
         {
             return Math.Abs(p2.X - p1.X) >= Math.Abs(p2.X - result.X) + Math.Abs(p1.X - result.X)
                 && Math.Abs(p2.Y - p1.Y) >= Math.Abs(p2.Y - result.Y) + Math.Abs(p1.Y - result.Y);
         }
 
-        private Vector2[] CreateProjectionOnOXY(Vector3[] points)
+        private Point[] CreateProjectionOnOXY(Vector3D[] radiusVectors)
         {
-            return points.Select(p => new Vector2(p.X, p.Y)).ToArray();
+            return radiusVectors.Select(p => new Point(p.X, p.Y, 0)).ToArray();
         }
 
-        private Vector2 CreateProjectionOnOXY(Vector3 p) => new Vector2(p.X, p.Y);
+        private Point CreateProjectionOnOXY(Vector3D radiusVector) => new Point(radiusVector.X, radiusVector.Y, 0);
 
-        private bool CheckCoplanarity(Vector3[] vectors, Vector3 currentVector)
+        private bool CheckCoplanarity(Vector3D[] radiusVectors, Vector3D currentRadiusVector)
         {
-            for (int i = 0; i < vectors.Count(); i++)
+            for (int i = 0; i < radiusVectors.Count(); i++)
             {
-                if (CheckCoplanarity(vectors[i], vectors[(i + 1) % vectors.Count()], currentVector))
+                if (CheckCoplanarity(radiusVectors[i], radiusVectors[(i + 1) % radiusVectors.Count()], currentRadiusVector))
                 {
                     return true;
                 }
@@ -86,17 +87,17 @@ namespace EnergomerWebApp.Services.Impl
             return false;
         }
 
-        private bool CheckCoplanarity(Vector3 v1, Vector3 v2, Vector3 v3)
+        private bool CheckCoplanarity(Vector3D v1, Vector3D v2, Vector3D v3)
         {
-            return Vector3.Dot(v1 * v2, v3) == 0f;
+            return (v1 * v2).Dot(v3) == 0;
         }
 
-        private Vector3[] Sort(Vector3[] vectors)
+        private Vector3D[] Sort(Vector3D[] vectors)
         {
             for (var i = 0; i < vectors.Count() - 1; i++)
             {
                 int? index = null;
-                float minDistance = 0;
+                double minDistance = 0;
                 for (var j = i + 1; j < vectors.Count(); j++)
                 {
                     var distance = (vectors[i] - vectors[j]).Length();
@@ -119,17 +120,16 @@ namespace EnergomerWebApp.Services.Impl
             return vectors;
         }
 
-        private Vector3 ToVector(GeoCoordinate point)
+        private Vector3D ToVector(GeoCoordinate point)
         {
 
             double alfa = point.Longitude * (Math.PI / 180.0);
             double beta = point.Latitude * (Math.PI / 180.0);
-            return new Vector3()
-            {
-                X = (float)(Math.Cos(alfa) * Math.Cos(beta)),
-                Y = (float)(Math.Sin(alfa) * Math.Cos(beta)),
-                Z = (float)Math.Sin(beta)
-            };
+            double X = Math.Cos(alfa) * Math.Cos(beta);
+            double Y = Math.Sin(alfa) * Math.Cos(beta);
+            double Z = Math.Sin(beta);
+
+            return new Vector3D(X, Y, Z);
         }
     }
 }
